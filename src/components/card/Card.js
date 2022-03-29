596c8b5bc3b947ee2805aaf6db5927cec0c83bb0
Omer-Cicek <omercicek160408@gmail.com>
2022-03-29 23:51:23 +0300

    last commit

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,45 +1,54 @@
 import './Card.scss';
-import { useState } from 'react';
 import { categories } from '../../helper/data';
+import { useState } from 'react';
+
+const Itemss = ({ name, image, options }) => {
+  const [content, setContent] = useState(true);
+  return (
+    <div>
+      {content ? (
+        <div
+          className="card-container-div"
+          onClick={() => setContent(!content)}
+        >
+          <img src={image} alt="" className="card-container-img" />
+          <div className="card-container-p">
+            <p className="card-container-p">{name}</p>
+          </div>
+        </div>
+      ) : (
+        <div
+          onClick={() => setContent(!content)}
+          className="card-container-div"
+        >
+          <div className="card-container-p">
+            <p>{options[0]}</p>
+          </div>
+          <div className="card-container-p">
+            <p>{options[1]}</p>
+          </div>
+          <div className="card-container-p">
+            <p>{options[2]}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 const Card = () => {
-  console.log(categories);
-  const [content, setContent] = useState(true);
-
-  const clickHandler = () => {
-    content ? setContent(false) : setContent(true);
-  };
-
   return (
-    <div className="card-container" onClick={clickHandler}>
-      {content &&
-        categories.map((item, idx) => {
-          return (
-            <ul key={idx} className="card-container-ul">
-              <img src={item.img} alt="" className="card-container-img" />
-              <li className="card-container-p">
-                <p className="card-container-p">{item.name}</p>
-              </li>
-            </ul>
-          );
-        })}
-      {!content &&
-        categories.map((item, idx) => {
-          const { options } = item;
-          return (
-            <ul key={idx} className="card-container-ul">
-              <li className="card-container-p">
-                <p>{options[0]}</p>
-              </li>
-              <li className="card-container-p">
-                <p>{options[1]}</p>
-              </li>
-              <li className="card-container-p">
-                <p>{options[2]}</p>
-              </li>
-            </ul>
-          );
-        })}
+    <div className="card-container">
+      {categories.map((item, index) => {
+        return (
+          <Itemss
+            name={item.name}
+            image={item.img}
+            options={item.options}
+            // key={index}
+          />
+        );
+      })}
     </div>
   );
 };
